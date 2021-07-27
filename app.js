9e89ced4fce64012654bf83279dd95e91558f2de
Rohan <rohank2502@gmail.com>
2021-07-27 15:21:42 +0530

    app

--- app.js
+++ app.js
@@ -10,7 +10,10 @@ app.use(express.static(`${__dirname}/public`))
 
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
+  pool: true,
   auth: {
     user: `${process.env.EMAIL}`,
     pass: `${process.env.PASSWORD}`,
